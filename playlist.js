f81256f2677e2427fb98c8aa3b7584ba97d9edb3
Leda BCN <leda.trifonova@gmail.com>
2022-09-19 06:46:08 +0200

    add a song to playlist ul.appendChild

--- playlist.js
+++ playlist.js
@@ -1,8 +1,21 @@
 window.onload = init;
+// first the page must load and the function init will be triggered, action will be triggered *after loading*
 function init() {
     var button = document.getElementById("addButton");
+    //localize the button via id"addButton" and trigger function handleButtonClick, which is show msg alert "Button was clicked!"
     button.onclick = handleButtonClick;
+    //handleButtonClick is what happens when the button is clicked, which is specified via the function below
 }
 function handleButtonClick() {
-    alert("Button was clicked!");
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.value;
+    if (songName == "") {
+        alert("Please enter a song");
+    } else {
+    alert("Adding " + songName);
+}
+   var li = document.createElement("li");
+   li.innerHTML = songName;
+   var ul = document.getElementById("playlist");
+   ul.appendChild(li);
 }
