da178be8137080ad9931c62232f1355b25510a0e
Leda BCN <leda.trifonova@gmail.com>
2022-09-27 06:12:02 +0200

    fix a bug

--- myLoc.js
+++ myLoc.js
@@ -1,19 +1,32 @@
+var ourCoords = {
+    latitude: 47.624851,
+    longitude: -122.52099
+};
+
 window.onload = getMyLocation;
 
 function getMyLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+
+        navigator.geolocation.getCurrentPosition(
+            displayLocation, 
+            displayError);
     } else {
-        alert("Ops, no geolocation support");
+        alert("Oops, no geolocation support");
     }
 }
 function displayLocation(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
-    var div = document.getElementById("location"),
+    var div = document.getElementById("location");
     div.innerHTML = "You are at Latitude: " + latitude + " , Longitude: " + longitude;
+
+    var km = computeDistance(position.coords, ourCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You're " + km + " km from WickedlySmart HQ.";
 }
+
 function displayError(error) {
     var errorTypes = {
         0: "Unknown error",
@@ -27,5 +40,23 @@ function displayError(error) {
     }
     var div = document.getElementById("location");
     div.innerHTML = errorMessage;
+    }
+    function computeDistance(startCoords, destCoords) {
+        var startLatRads = degreesToRadians(startCoords.latitude);
+        var startLongRads = degreesToRadians(startCoords.longitude);
+        var destLatRads = degreesToRadians(destCoords.latitude);
+        var destLongRads = degreesToRadians(destCoords.longitude);
+    
+        var Radius = 6371; // radius of the Earth in km
+        var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+                        Math.cos(startLatRads) * Math.cos(destLatRads) *
+                        Math.cos(startLongRads - destLongRads)) * Radius;
+    
+        return distance;
+    }
+    
+    function degreesToRadians(degrees) {
+        radians = (degrees * Math.PI)/180;
+        return radians;
     }
     
